94e80c2b41a13a96d3f8daa95ca1f39ee580f4dd
CHIRAG-YADAV-Png <chiragyadav.ary22@gmail.com>
2026-02-10 20:21:31 +0530

    adding query selector in side2 and adding event listener

---
+++ script.js
@@ -0,0 +1,29 @@
+let events = [];
+
+const form = document.querySelector("form");
+const eventContainer = document.querySelector(".side2");
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addEvent();
+});
+
+
+function addEvent() {
+    const title = document.getElementById("event_title").value.trim();
+    const date = document.getElementById("event_date").value;
+    const category = document.getElementById("category").value;
+    const description = document.getElementById("description").value.trim();
+
+    if (!title || !date) {
+        alert("Please fill Event Title and Date!");
+        return;
+    }
+
+    const eventObj = {
+        title,
+        date,
+        category,
+        description
+    };
